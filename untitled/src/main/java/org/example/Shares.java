e488e95fafe18f11fa6f776f2ecc54e1dc8555b1
yehor_0 <i2235641618i@gmail.co,m>
2022-11-07 15:38:45 +0200

    All ( problem with selling)

--- untitled/src/main/java/org/example/Shares.java
+++ untitled/src/main/java/org/example/Shares.java
@@ -8,11 +8,12 @@ public class Shares {
         this.ID = ID;
         try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Project", "root", "561151181Yehor*")) {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("Select Name , Price From Shares Where OwnerID = " + ID + ";" );
+            ResultSet rs = stmt.executeQuery("Select Name , Price , ID From Shares Where OwnerID = " + ID + ";" );
             System.out.println("Your Shares : ");
             while (rs.next()) {
                 System.out.println("Name : " + rs.getString(1));
-                System.out.println("Price : " + rs.getInt(2) + "$\n");
+                System.out.println("Price : " + rs.getInt(2) + "$");
+                System.out.println("Write if you want to sell  : " + rs.getInt(3) + "\n");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -55,6 +56,36 @@ public class Shares {
             System.out.println("Your Account after buying : " + (account - price));
             try {
                 boolean rs = stmt.execute("Update Profile Set Account = " + (account - price) + " Where ID=" + ID );
+            }catch (Exception e) {
+                System.out.println("Insufficient funds");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void SellShares(int ID , int IDShares) {
+        int price = 0;
+        int account = 0;
+        try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Project", "root", "561151181Yehor*")) {
+            Statement stmt = con.createStatement();
+            boolean rs = stmt.execute("Update Shares Set OwnerID = null  Where ID =" + IDShares + ";");
+            System.out.println("Successful");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Project", "root", "561151181Yehor*")) {
+            Statement stmt = con.createStatement();
+            ResultSet resPrice = stmt.executeQuery("Select * From Shares Where ID =" + IDShares );
+            while (resPrice.next()) {
+                price = resPrice.getInt(3);
+            }
+            ResultSet resAccount = stmt.executeQuery("Select * From Profile Where ID =" + ID );
+            while (resAccount.next()) {
+                account = resAccount.getInt(4);
+            }
+            System.out.println("Your Account after buying : " + (account + price));
+            try {
+                boolean rs = stmt.execute("Update Profile Set Account = " + (account + price) + " Where ID=" + ID );
             }catch (Exception e) {
                 System.out.println("Insufficient funds");
             }
